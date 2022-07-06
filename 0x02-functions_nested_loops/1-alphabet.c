b357aba3f87364f91d4840a0aa29351bb7e6b013
Saintvic2020 <saintvic2020@gmail.com>
2022-07-05 21:02:09 -0700

    nest

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: always return 0
+*/
+
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+printf("%d\n", ch);
+c++
+}
+return (0);
+}
