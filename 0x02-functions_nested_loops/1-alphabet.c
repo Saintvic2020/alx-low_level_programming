92113c6e1710e905abe51d6a7fade1e80ef66419
Saintvic2020 <saintvic2020@gmail.com>
2022-07-06 21:17:42 -0700

    nest

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
-* print_alphabet - Make the alphabet
+* print_alphabet - function to print abc
 *
-* Return: always return 0
+* Return: 0
 */
-
 void print_alphabet(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(c)
+_putchar(c);
 }
 _putchar('\n');
-}
-return (0);
 }
