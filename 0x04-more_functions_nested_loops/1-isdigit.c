19a580db181e250b14d8848e06c74caac5832349
Saintvic2020 <saintvic2020@gmail.com>
2022-07-08 20:12:49 -0700

    nest

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /*
 * _isdigit - checks for a digit (0 through 9)
 * @c: int to be checked
@@ -6,7 +7,7 @@
 */
 int _isdigit(int c)
 {
-if (c >= '48' && c <= '57');
+if (c >= '0' && c <= '9');
 {
 return (1);
 }
