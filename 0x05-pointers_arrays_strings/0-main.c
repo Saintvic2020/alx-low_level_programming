8f0b76b1c1af6d0b2f92eb5c66b5e40847185396
Saintvic2020 <saintvic2020@gmail.com>
2022-07-12 07:29:06 -0700

    array

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -6,13 +6,8 @@
 *
 * Return: Always 0.
 */
-int main(void)
-{
-int n;
 
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
+void reset_to_98 (int *n)
+{
+	*n = 98;
 }
