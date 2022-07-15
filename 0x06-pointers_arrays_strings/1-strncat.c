8b3d12890ddc96c08e0cd34c263998ce66bc2802
Saintvic2020 <saintvic2020@gmail.com>
2022-07-14 22:12:05 -0700

    strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,14 +10,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-	for (i = 0; dest[i] != '\0'; i++)
-	{
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+{
 
-	}
-	for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
-	{
-		dest[i] = src[j];
-	}
-	return (dest);
+}
+for (j = 0; src[j] != '\0' && n > 0; j++, n--, i++)
+{
+dest[i] = src[j];
+}
+return (dest);
 }
